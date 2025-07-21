ca06e7706d390e96e07da0f80aefdd4de4f6a052
hyeonjin6767 <parkhj067670@gmail.com>
2025-07-21 09:47:46 +0900

    Initial Commit

---
+++ SeSACWeek4Codebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACWeek4Codebase
+//
+//  Created by 박현진 on 7/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
